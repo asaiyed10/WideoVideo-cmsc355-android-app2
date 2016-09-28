9fa208872fb2441f5d6c7b26af759035b735bf10
mantvu <vumt2@vcu.edu>
2016-09-28 00:58:57 -0400

    commit

--- app/src/main/java/cmsc355wideo/wideo_1/CameraActivity.java
+++ app/src/main/java/cmsc355wideo/wideo_1/CameraActivity.java
@@ -1,8 +1,37 @@
 package cmsc355wideo.wideo_1;
 
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
 /**
  * Created by mantvu on 9/28/2016.
  */
 
-public class CameraActivity {
+public class CameraActivity extends Activity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_preview);
+
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+    }
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
 }
--- app/src/main/java/cmsc355wideo/wideo_1/MainActivity.java
+++ app/src/main/java/cmsc355wideo/wideo_1/MainActivity.java
@@ -1,8 +1,11 @@
 package cmsc355wideo.wideo_1;
 
+import android.content.Intent;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_record = (Button) findViewById(R.id.button);
+        Button button_gallery = (Button) findViewById(R.id.button2);
+        Button button_settings = (Button) findViewById(R.id.button3);
+        Button button_quit = (Button) findViewById(R.id.button4);
 
+        button_quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+        button_record.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent camera = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(camera);
+            }
+        });
     }
 
 }
