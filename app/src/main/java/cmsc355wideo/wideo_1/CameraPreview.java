5792f2c62237452408c4fa7df983149e68698b13
mantvu <vumt2@vcu.edu>
2016-10-04 20:08:19 -0400

    camera works for api 21, i also made things look a bit nicer

--- app/src/main/java/cmsc355wideo/wideo_1/CameraPreview.java
+++ app/src/main/java/cmsc355wideo/wideo_1/CameraPreview.java
@@ -19,19 +19,17 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
      private Camera mCamera;
 
      public CameraPreview(Context context, Camera camera) {
-        super(context);
-        mCamera = camera;
+         super(context);
+         mCamera = camera;
 
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = getHolder();
+
+         mHolder = getHolder();
          mHolder.addCallback(this);
-         // deprecated setting, but required on Android versions prior to 3.0
          mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         }
 
     public void surfaceCreated(SurfaceHolder holder) {
-         // The Surface has been created, now tell the camera where to draw the preview.
+
         try {
              mCamera.setPreviewDisplay(holder);
              mCamera.startPreview();
@@ -42,13 +40,11 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     }
         public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
+
          }
 
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-         // If your preview can change or rotate, take care of those events here.
-       // Make sure to stop the preview before resizing or reformatting it.
 
         if (mHolder.getSurface() == null){
             // preview surface does not exist
@@ -59,11 +55,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
              mCamera.stopPreview();
             } catch (Exception e){
-            // ignore: tried to stop a non-existent preview
            }
-
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
 
         // start preview with new settings
          try {
