34a4ae68191ae2c225c96d83673df525d0641a82
salmansa <salmans@vcu.edu>
2016-10-06 16:44:49 -0400

    Refactored the test class

--- app/src/main/java/cmsc355wideo/wideo_1/MainActivity.java
+++ app/src/main/java/cmsc355wideo/wideo_1/MainActivity.java
@@ -1,12 +1,10 @@
 package cmsc355wideo.wideo_1;
 
 import android.content.Intent;
-import android.hardware.Camera;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
