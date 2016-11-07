e852d62ed510c5695067b382ec69a674d56fc7d4
mantvu <vumt2@vcu.edu>
2016-11-06 22:54:50 -0500

    change contact intent

--- app/src/main/java/Cmsc355Wideo/Wideo/MainActivity.java
+++ app/src/main/java/Cmsc355Wideo/Wideo/MainActivity.java
@@ -4,6 +4,8 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -11,6 +13,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
-    public static final int PICK_ACTION = 1;
+    public static final int PICK_MEDIA = 1;
+    public static final int PICK_CONTACT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent flist = new Intent(Intent.ACTION_PICK,
                         ContactsContract.Contacts.CONTENT_URI);
-                startActivity(flist);
+                startActivityForResult(flist,PICK_CONTACT);
                 }
         });
         buttonGallery.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent gallery = new Intent();
                 gallery.setType("image/*");
                 gallery.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(gallery,"Select"),PICK_ACTION);
+                startActivityForResult(Intent.createChooser(gallery,"Select"),PICK_MEDIA);
             }
         });
         buttonSettings.setOnClickListener(new View.OnClickListener() {
@@ -75,7 +79,35 @@ public class MainActivity extends AppCompatActivity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_CONTACT) {
+            if (resultCode == RESULT_OK) {
+                Uri contactUri = data.getData();
+                Cursor cs = managedQuery(contactUri, null, null, null, null);
+                if (cs.moveToFirst()) {
+                    String contactId =
+                            cs.getString(cs.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                    String hasPhoneNo = cs.getString(
+                            cs.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                    if (hasPhoneNo.equalsIgnoreCase("1")) {
+                        Cursor phoneCursor = getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId,
+                                null, null);
+                        phoneCursor.moveToFirst();
+                        String phoneNumber = phoneCursor.getString(
+                                phoneCursor.getColumnIndex("data1"));
+                        String contactName = cs.getString(
+                                cs.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        Toast.makeText(this, "Contact picked: " + contactName + "\n" + phoneNumber,
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
 
+            }
+        }
+    }
 }
 
 
